fa6ab14223952a8450f3ca285ac7b2d89766031f
thoser666 <thoser666@web.de>
2024-04-22 19:33:56 +0200

    unchanged Version

--- src/base/util/mod.rs
+++ src/base/util/mod.rs
@@ -2,3 +2,4 @@ pub(crate) mod date_time_offset_extensions;
 mod dictionary_extensions;
 mod http_rate_limited_rest_request_exception;
 mod http_response_message_extensions;
+mod http_rest_request_exception;
